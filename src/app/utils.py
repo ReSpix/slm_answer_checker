4658e5be96112ffd9223b2b78dce56c8e93530f5
ReSpix <yanovich.yakov2@gmail.com>
2026-07-03 15:40:59 +1000

    Move write_file_with_uuid to utils

--- src/app/utils.py
+++ src/app/utils.py
@@ -1,6 +1,21 @@
 from pathlib import Path
+import uuid
 
-def write_file(file_path: str, data: str, encoding: str = 'utf8') -> None:
+
+def write_file(file_path: str, data: str, encoding: str = "utf8") -> None:
     path = Path(file_path)
     path.parent.mkdir(parents=True, exist_ok=True)
     path.write_text(data, encoding=encoding)
+
+
+def write_file_with_uuid(dir: str, content: str, suffix=".json", encoding="utf-8"):
+    path = Path(dir)
+
+    path.mkdir(parents=True, exist_ok=True)
+
+    unique_name = f"{uuid.uuid4().hex}{suffix}"
+    unique_path = path / unique_name
+
+    unique_path.write_text(content, encoding)
+
+    return str(unique_path)
